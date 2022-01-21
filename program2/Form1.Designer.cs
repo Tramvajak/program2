68cad3935aa73627b063db7b40d8144a259ff4b5
Tramvajak <kutelvas1@gmail.com>
2022-01-21 20:55:49 +0400

    Добавьте файлы проекта.

---
+++ program2/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace program2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_SelectFile = new System.Windows.Forms.Button();
+            this.text_FilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ColumnAlgorithms = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnMode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnPadding = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnEnDeCrypt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_SelectFile
+            // 
+            this.btn_SelectFile.Location = new System.Drawing.Point(383, 12);
+            this.btn_SelectFile.Name = "btn_SelectFile";
+            this.btn_SelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_SelectFile.TabIndex = 0;
+            this.btn_SelectFile.Text = "Select";
+            this.btn_SelectFile.UseVisualStyleBackColor = true;
+            this.btn_SelectFile.Click += new System.EventHandler(this.btn_SelectFile_Click);
+            // 
+            // text_FilePath
+            // 
+            this.text_FilePath.Location = new System.Drawing.Point(116, 14);
+            this.text_FilePath.Name = "text_FilePath";
+            this.text_FilePath.ReadOnly = true;
+            this.text_FilePath.Size = new System.Drawing.Size(261, 20);
+            this.text_FilePath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File Name";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(434, 71);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 3;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnAlgorithms,
+            this.ColumnMode,
+            this.ColumnPadding,
+            this.ColumnEnDeCrypt,
+            this.ColumnTime});
+            this.dataGridView1.Location = new System.Drawing.Point(45, 132);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(545, 295);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // ColumnAlgorithms
+            // 
+            this.ColumnAlgorithms.HeaderText = "Algorithms";
+            this.ColumnAlgorithms.Name = "ColumnAlgorithms";
+            this.ColumnAlgorithms.ReadOnly = true;
+            // 
+            // ColumnMode
+            // 
+            this.ColumnMode.HeaderText = "Mode";
+            this.ColumnMode.Name = "ColumnMode";
+            this.ColumnMode.ReadOnly = true;
+            // 
+            // ColumnPadding
+            // 
+            this.ColumnPadding.HeaderText = "Padding";
+            this.ColumnPadding.Name = "ColumnPadding";
+            this.ColumnPadding.ReadOnly = true;
+            // 
+            // ColumnEnDeCrypt
+            // 
+            this.ColumnEnDeCrypt.HeaderText = "En/De crypt";
+            this.ColumnEnDeCrypt.Name = "ColumnEnDeCrypt";
+            this.ColumnEnDeCrypt.ReadOnly = true;
+            // 
+            // ColumnTime
+            // 
+            this.ColumnTime.HeaderText = "Time";
+            this.ColumnTime.Name = "ColumnTime";
+            this.ColumnTime.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text_FilePath);
+            this.Controls.Add(this.btn_SelectFile);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_SelectFile;
+        private System.Windows.Forms.TextBox text_FilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnAlgorithms;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnMode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnPadding;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnEnDeCrypt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTime;
+    }
+}
+
---
+++ program2/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btn_Start.Enabled = false;
+        }
+
+        string path;
+
+
+        private string Text = null;
+
+
+        private void btn_SelectFile_Click(object sender, EventArgs e) // диалог выбора файла
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                path = openFileDialog1.FileName;
+                text_FilePath.Text = path;
+                using (StreamReader stream = new StreamReader(path))
+                {
+                    Text = stream.ReadToEnd();
+                }
+                btn_Start.Enabled = true;
+            }
+        }
+        CipherMode[] Modes = { CipherMode.ECB,CipherMode.CBC,CipherMode.CFB,CipherMode.OFB,CipherMode.CTS};
+        PaddingMode[] Paddings = { PaddingMode.PKCS7, PaddingMode.Zeros, PaddingMode.None };
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+
+            Start(_DES);
+            Start(_TripleDES);
+            Start(_Rijndael);
+            Start(_RC2);
+
+
+            //Encrypt(_Rijndael, key, iv, CipherMode.OFB, PaddingMode.Zeros);
+        }
+
+        private void Start(string alg) // перебор всех mode and padding с таймером выполнения, шифрофка, дешифровка и внесение результаты в таблицу
+        {
+            for (int i = 0; i < Modes.Length; i++)
+            {
+                for (int j = 0; j < Paddings.Length; j++)
+                {
+                    try
+                    {
+                        byte[] key = GenKey(alg);
+                        byte[] iv = GenIV(alg);
+
+                        TimeSpan time = Timer(() => Encrypt(alg, key, iv, Modes[i], Paddings[j]));
+                        dataGridView1.Rows.Add(alg, Modes[i].ToString(), Paddings[j].ToString(),"Encrypt", time.TotalMilliseconds.ToString());
+
+                        time = Timer(() => Decrypt(alg, key, iv, Modes[i], Paddings[j]));
+                        dataGridView1.Rows.Add(alg, Modes[i].ToString(), Paddings[j].ToString(),"Decrypt", time.TotalMilliseconds.ToString());
+
+                    }
+                    catch (Exception)
+                    {
+                        dataGridView1.Rows.Add(alg, Modes[i].ToString(), Paddings[j].ToString(),"", "Err");
+                    }
+                    
+                }
+            }
+        }
+
+        private void Encrypt(string algorithm, byte[] Key, byte[] IV, CipherMode Mode, PaddingMode Padding) // шифрование
+        {
+            SymmetricAlgorithm sa = CreateSymmetricAlgorithm(algorithm);
+
+            sa.Key = Key;
+            sa.IV = IV;
+
+            sa.Mode = Mode;
+            sa.Padding = Padding;
+
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, sa.CreateEncryptor(),CryptoStreamMode.Write);
+
+            byte[] plainbytes = Encoding.UTF8.GetBytes(Text);
+            cs.Write(plainbytes, 0, plainbytes.Length);
+            cs.Close();
+            byte[] cipherbytes = ms.ToArray();
+            ms.Close();
+
+            Debug.WriteLine("ENCRYPT ALGORITM:" + algorithm + "; MODE:" + Mode.ToString() + "; PADDING:" + Padding.ToString());
+            DebugHash(cipherbytes);
+        }
+        private void Decrypt(string algorithm, byte[] Key, byte[] IV, CipherMode Mode, PaddingMode Padding) // дешифрование
+        {
+            SymmetricAlgorithm sa = CreateSymmetricAlgorithm(algorithm);
+
+            sa.Key = Key;
+            sa.IV = IV;
+
+            sa.Mode = Mode;
+            sa.Padding = Padding;
+
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, sa.CreateEncryptor(), CryptoStreamMode.Write);
+
+            byte[] plainbytes = Encoding.UTF8.GetBytes(Text);
+            cs.Write(plainbytes, 0, plainbytes.Length);
+            cs.Close();
+            byte[] cipherbytes = ms.ToArray();
+            ms.Close();
+
+            Debug.WriteLine("DECRYPT ALGORITM:" + algorithm + "; MODE:" + Mode.ToString() + "; PADDING:" + Padding.ToString());
+            DebugHash(cipherbytes);
+        }
+
+        private byte[] GenIV(string alg) // новый рандомный вектор
+        {
+            //Generate new random IV
+            SymmetricAlgorithm sa = CreateSymmetricAlgorithm(alg);
+            sa.GenerateIV();
+            return sa.IV;
+            
+        }
+        private byte[] GenKey(string alg) // новый рандомный ключ
+        {
+            //Generate new random IV
+            SymmetricAlgorithm sa = CreateSymmetricAlgorithm(alg);
+            sa.GenerateKey();
+            return sa.Key;
+        }
+
+
+
+
+
+        SymmetricAlgorithm CreateSymmetricAlgorithm(string algoritm) // выбор алгоритма шифрование
+        {
+            //create new instance of symmetric algorithm
+            if (algoritm == _RC2)
+                return RC2.Create();
+            if (algoritm == _Rijndael)
+                return Rijndael.Create();
+            if (algoritm == _DES)
+                return DES.Create();
+            if (algoritm == _TripleDES)
+                return TripleDES.Create();
+            return null;
+        }
+        private const string _RC2 = "RC2";
+        private const string _Rijndael = "Rijndael";
+        private const string _DES = "DES";
+        private const string _TripleDES = "TripleDES";
+
+        private TimeSpan Timer(Action action) // таймер выполнения
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            action();
+            watch.Stop();
+            return watch.Elapsed;
+        }
+        /// <summary>
+        /// Debug Write Line array Hash
+        /// </summary>
+        /// <param name="hash">Hash</param>
+        private void DebugHash(byte[] hash) 
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+                sb.Append(String.Format("{0,2:X2} ", hash[i]));
+            Debug.WriteLine(sb.ToString());
+        }
+
+    }
+}
